3b373ce7a2f821fc03a9776092c4020f4e3e8975
abdvkh <e-address@bk.ru>
2021-01-01 11:28:28 +0500

    Show users in the table from DB

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require 'inc/utils.php';
+require 'db/utils.php';
 
 $logged = $_REQUEST['logged'] ?? false;
 
@@ -7,18 +8,42 @@ if (!$logged) {
 	redirect('login.php');
 }
 
+$db_data = get_db();
+$users_data = $db_data['users'];
 ?>
 
 <?php include 'components/heading.php';?>
 
 <div class="container">
 	<div class="row justify-content-center">
-
 		<div class="col-8" style="margin-top: 100px">
-
-		  	<div class="card card-block">
-		  		<h1>Welcome!</h1>
-		  	</div>
+            <div class="card p-4">
+                <div class="card-header">Admin panel</div>
+                    <?php if (count($users_data) > 0): ?>
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Firstname</th>
+                                <th scope="col">Lastname</th>
+                                <th scope="col">email</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($users_data as $key => $user):?>
+                                    <tr>
+                                        <th scope="row"><?php echo ++$key ?></th>
+                                        <td><?php echo $user['firstname'] ?></td>
+                                        <td><?php echo $user['lastname'] ?></td>
+                                        <td><?php echo $user['email'] ?></td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    <?php else: ?>
+                        <h5 class="p-3 text-center">Hey, there are no users</h5>
+                    <?php endif; ?>
+            </div>
 		</div>
 	</div>
 </div>

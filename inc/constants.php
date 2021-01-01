2abce8294b123a3d2a0f867ba6b488377b9fc65e
abdvkh <e-address@bk.ru>
2021-01-01 16:18:45 +0500

    Change of 'is required'

--- inc/constants.php
+++ inc/constants.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_PATH', 'db/db.json');
 define('UPLOADS_PATH', 'uploads/');
-define('IS_REQUIRED', ' is necessary');
+define('IS_REQUIRED', ' is required');
 define('HOMEPAGE', 'index.php');
 define('REGISTRATION_PAGE', 'register.php');
 define('LOGIN_PAGE', 'login.php');

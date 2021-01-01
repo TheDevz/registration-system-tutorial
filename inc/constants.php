8a9fb132d273312f162e3c061ea40ae4e8a23ad8
abdvkh <e-address@bk.ru>
2021-01-01 12:25:25 +0500

    Add remaining endpoints

--- inc/constants.php
+++ inc/constants.php
@@ -2,3 +2,5 @@
 define('DB_PATH', 'db/db.json');
 define('IS_REQUIRED', ' is necessary');
 define('HOMEPAGE', 'index.php');
+define('REGISTRATION_PAGE', 'register.php');
+define('LOGIN_PAGE', 'login.php');
